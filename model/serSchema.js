6c9b54de5dfd136bb494523f3abc5710ed1de159
siddhartha malik <132971656+sidddmalik@users.noreply.github.com>
2024-03-17 13:21:25 +0530

    Update serSchema.js

--- model/serSchema.js
+++ model/serSchema.js
@@ -4,8 +4,8 @@ const serSchema = new mongoose.Schema({
     "Name":{type:String},
     "ID":{type:String},
     "Phone No.":{type:String},
-    "Sertype":{type:String},
-    "Seroff":{type:String},
+    "EmailId":{type:String},
+    "Sercategory":{type:String},
     "Pincode":{type:String},
 
     },{

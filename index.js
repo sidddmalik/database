7fb915c47288dfe99b69a6f5e0481c2051cab749
siddhartha malik <132971656+sidddmalik@users.noreply.github.com>
2024-03-17 11:35:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
-const petRoute = require("./controller/petRoute");
-const consumerRoute = require("./controller/consumerRoute");
+const conRoute = require("./controller/conRoute");
+const serRoute = require("./controller/serRoute");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -12,7 +12,7 @@ const app = express();
 
 
 mongoose.set("strictQuery",true); //Depercation Warning //To supress the warning
-mongoose.connect("mongodb+srv://user1:12345@cluster0.hbxb7a5.mongodb.net/petdb");
+mongoose.connect("mongodb+srv://siddharthamalik2021:1234@cluster0.5douab4.mongodb.net/");
 var db = mongoose.connection;
 db.on("open",()=>console.log("Connected to DB"));
 db.on("error",()=>console.log("Error occured"));
@@ -20,8 +20,8 @@ db.on("error",()=>console.log("Error occured"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
-app.use("/petRoute",petRoute);
-app.use("/consumerRoute",consumerRoute);
+app.use("/conRoute",conRoute);
+app.use("/serRoute",serRoute);
 
 app.listen(4000,()=>{
     console.log("Server started at 4000")

b252c8d1c0319e687ca03cca5bb1bed681a5ce6c
siddhartha malik <132971656+sidddmalik@users.noreply.github.com>
2024-03-17 11:39:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 mongoose.set("strictQuery",true); //Depercation Warning //To supress the warning
-mongoose.connect("mongodb+srv://siddharthamalik2021:1234@cluster0.5douab4.mongodb.net/");
+mongoose.connect("mongodb+srv://siddharthamalik2021:1234@cluster0.5douab4.mongodb.net/condb");
 var db = mongoose.connection;
 db.on("open",()=>console.log("Connected to DB"));
 db.on("error",()=>console.log("Error occured"));
